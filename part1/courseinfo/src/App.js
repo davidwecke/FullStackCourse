7e4bcc56d4826cdbe718b9ada79abe2da3377a70
David Wecke <davidjohann1@gmail.com>
2022-10-25 13:44:37 -0400

    Pass array as only prop

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-    <Part part={props.part1} />
-    <Part part={props.part2} />
-    <Part part={props.part3}/>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]}/>
     </>
   )
 }
@@ -27,7 +27,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of Exercises {props.total}</p>
+      <p>Number of Exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </>
   )
 }
@@ -52,8 +52,8 @@ const App = () => {
   return (
     <div>
       <Course course={course}/>
-      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]} />
-      <Total total={parts[0].exercises+parts[1].exercises+parts[2].exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
