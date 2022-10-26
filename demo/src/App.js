78519d525656a1a937174f5b370031304f9d9153
David Wecke <davidjohann1@gmail.com>
2022-10-26 08:02:53 -0400

    Complex state in demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
+const Display = ({counter}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const [clicks, setClicks] = useState({left:0,right:0})
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
+
+  return (
+    <div>
+      {clicks.left}
+      <Button onClick={handleLeftClick} text="left"/>
+      <Button onClick={handleRightClick} text="right"/>
+      {clicks.right}
     </div>
-  );
+  )
 }
 
 export default App;
