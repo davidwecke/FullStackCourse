f883cd90d2f8eddb6958a1e7e341ca086b825050
David Wecke <davidjohann1@gmail.com>
2022-10-26 14:07:56 -0400

    Demo for part 2

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,65 +1,14 @@
-import {useState} from 'react'
+import Note from './components/Note'
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-
-const Button = ({onClick, text}) => {
-  return(
-    <button onClick={onClick}>{text}</button>
-  )
-}
-
-const History = ({allClicks}) => {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing buttons
-      </div>
-    )
-  }
-
+const App = ({notes}) => {
   return (
     <div>
-      btton press history: {allClicks.join(' ')}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => <Note key={note.id} note={note} />  )}
+      </ul>
     </div>
   )
 }
 
-const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAllClicks] = useState([])
-
-  const [clicks, setClicks] = useState({left:0,right:0})
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-
-  const handleLeftClick = () => {
-    setLeft(left+1)
-    setAllClicks(allClicks.concat('L'))
-  }
-
-  const handleRightClick = () => {
-    setRight(right+1)
-    setAllClicks(allClicks.concat('R'))
-  }
-
-  return (
-    <div>
-      {left}
-      <Button onClick={handleLeftClick} text="left"/>
-      <Button onClick={handleRightClick} text="right"/>
-      {right}
-      <History allClicks={allClicks}/>
-    </div>
-  )
-}
-
-export default App;
+export default App
